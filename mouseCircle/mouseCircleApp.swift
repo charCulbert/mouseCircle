5ec31f788d40a41830a357f2a475ce38a12cecfa
char <charlieculbertmusic@gmail.com>
2024-08-30 01:54:49 +0200

    Initial Commit

---
+++ mouseCircle/mouseCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  mouseCircleApp.swift
+//  mouseCircle
+//
+//  Created by Charles Culbert on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct mouseCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
